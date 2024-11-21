d3d2fcafd4c4d2b3f48dc8ebf6867125118cebdf
EI <EI@LAB-SW3-04.ALUMNOS.LOCAL>
2024-11-20 19:28:32 -0500

    Mi primer error . Creacion de entities

---
+++ src/com/distribuida/entities/FacturaDetalle.java
@@ -0,0 +1,76 @@
+package com.distribuida.entities;
+
+public class FacturaDetalle {
+	
+	private int idFacturadetalle;
+	private int cantidad;
+	private double subtotal;
+	
+	private Factura factura;
+	private Libro libro;
+	
+	public FacturaDetalle() {
+		
+	}
+
+	public FacturaDetalle(int idFacturadetalle, int cantidad, double subtotal, Factura factura, Libro libro) {
+		
+		this.idFacturadetalle = idFacturadetalle;
+		this.cantidad = cantidad;
+		this.subtotal = subtotal;
+		this.factura = factura;
+		this.libro = libro;
+	}
+
+	public int getIdFacturadetalle() {
+		return idFacturadetalle;
+	}
+
+	public void setIdFacturadetalle(int idFacturadetalle) {
+		this.idFacturadetalle = idFacturadetalle;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public double getSubtotal() {
+		return subtotal;
+	}
+
+	public void setSubtotal(double subtotal) {
+		this.subtotal = subtotal;
+	}
+
+	public Factura getFactura() {
+		return factura;
+	}
+
+	public void setFactura(Factura factura) {
+		this.factura = factura;
+	}
+
+	public Libro getLibro() {
+		return libro;
+	}
+
+	public void setLibro(Libro libro) {
+		this.libro = libro;
+	}
+
+	@Override
+	public String toString() {
+		return "FacturaDetalle [idFacturadetalle=" + idFacturadetalle + ", cantidad=" + cantidad + ", subtotal="
+				+ subtotal + ", factura=" + factura + ", libro=" + libro + "]";
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ src/com/distribuida/principal/FacturaPrincipal.java
@@ -0,0 +1,27 @@
+package com.distribuida.principal;
+
+import com.distribuida.entities.Cliente;
+import com.distribuida.entities.Factura;
+
+public class FacturaPrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cliente cliente = new Cliente(1, "1751043140", "Alejandro","Imbaquingo","Puembo","0995843975","alejoxdeberes@outlook.com");
+		Cliente cliente2 = new Cliente(2, "172004313", "Roberto","Quinchuluisa","Tumbaco","094561234","quinchi@hotmail.com");
+		
+		
+		Factura factura=new Factura();
+		
+		factura.setIdFactura(1);
+		factura.setFecha(new Date());
+		factura.setNumFactura("FAC-0001");
+		factura.setTotalNeto(123.32);
+		factura.setIva(15.32);
+		factura.setTotal(138.64);
+		 
+		factura.setCliente(cliente);
+		System.out.println(factura.toString())
+	}
+
+}
