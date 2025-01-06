3735acc2a5b3af68d81f7c566635c25b57186cd7
EI <EI@LAB-SW3-04.ALUMNOS.LOCAL>
2025-01-06 18:41:39 -0500

    11

--- src/com/distribuida/entities/FacturaDetalle.java
+++ src/com/distribuida/entities/FacturaDetalle.java
@@ -1,26 +1,58 @@
 package com.distribuida.entities;
 
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 public class FacturaDetalle {
 	
-	private int idFacturadetalle;
-	private int cantidad;
-	private double subtotal;
+
+@Entity
+@Table(name="facturadetalle")
+public class FacturDetalle {
 	
-	private Factura factura;
+	///SQL : 1 A 1     <> JAVA = @OneToOne
+	/// SQL: 1 A N   <>  JAVA = @OneToMany   or @ManyToOne
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="idfacturadetalle")
+	private int idFacturadetalle;
+	@Column(name="cantidad")
+	private  int cantidad;
+	@Column(name=" subtotal")
+	private double subtotal;
+	@JoinColumn(name= "id_libro")
+	@ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
 	private Libro libro;
 	
-	public FacturaDetalle() {
+	@JoinColumn(name= "id_Factura")
+	@ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+	private Factura factura;
+	
+
+	
+	public FacturDetalle() {
 		
 	}
 
-	public FacturaDetalle(int idFacturadetalle, int cantidad, double subtotal, Factura factura, Libro libro) {
-		
+
+	public FacturDetalle(int idFacturadetalle, int cantidad, double subtotal, Libro libro, Factura factura) {
+		super();
 		this.idFacturadetalle = idFacturadetalle;
 		this.cantidad = cantidad;
 		this.subtotal = subtotal;
-		this.factura = factura;
 		this.libro = libro;
+		this.factura = factura;
 	}
+
 
 	public int getIdFacturadetalle() {
 		return idFacturadetalle;
@@ -73,4 +105,6 @@ public class FacturaDetalle {
 	
 	
 
+
+}
 }
